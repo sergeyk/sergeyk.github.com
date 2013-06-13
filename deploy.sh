075bab845a81bea723586feb2863a0aeeb36e374
Sergey Karayev <sergeykarayev@gmail.com>
2013-06-13 02:08:30 -0700

    fixed pt serif

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ if [ $CUR_BRANCH='source' ]; then
     cp -r _site/. .
     rm -r _site
     git add -A
-    git commit -am `git rev-parse --abbrev-ref HEAD`
+    git commit -am `git log --oneline -1`
     git push origin master
 fi
